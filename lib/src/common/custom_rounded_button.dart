51051a25cc0c141474d3136e8a75e6b62172fcb1
Ali-Amin <ali.m.amin98@gmail.com>
2019-04-27 14:47:09 +0200

    create custom rounded button widget

---
+++ lib/src/common/custom_rounded_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CustomRoundedButton extends StatelessWidget {
+  final String text;
+  final Function onPressed;
+  const CustomRoundedButton({Key key, @required this.text, this.onPressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 264,
+      height: 56,
+      child: FlatButton(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(200))),
+        child: Text("Sign In"),
+        color: Colors.black,
+        textColor: Colors.white,
+        onPressed: () {
+          onPressed();
+        },
+      ),
+    );
+  }
+}

9fe2a0ab6573c8b1096e7e6405a34cd01e420946
Ali-Amin <ali.m.amin98@gmail.com>
2019-04-27 12:28:51 +0200

    Create private constructor for profile

--- lib/src/models/profile.dart
+++ lib/src/models/profile.dart
@@ -1,9 +1,15 @@
 class Profile {
   String _uid;
-  String _userName;
   String _phoneNumber;
+  String _userName;
 
   String get uid => _uid;
-  String get userName => _userName;
   String get phoneNumber => _phoneNumber;
+  String get userName => _userName;
+
+  Profile._(
+    this._uid,
+    this._phoneNumber,
+    this._userName,
+  );
 }

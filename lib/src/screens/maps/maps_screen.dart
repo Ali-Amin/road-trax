1d9e6356929aea5b37a38266b00918e7c1e21f4c
KarimElghamry <Karimelghamry@gmail.com>
2019-04-27 15:15:14 +0200

    basic map functionality

--- lib/src/screens/maps/maps_screen.dart
+++ lib/src/screens/maps/maps_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapsScreen extends StatefulWidget {
   @override
@@ -9,10 +10,12 @@ class MapsScreen extends StatefulWidget {
 
 class _MapsScreenState extends State<MapsScreen> {
   Completer<GoogleMapController> _mapController;
+  Location _location;
 
   @override
   void initState() {
     _mapController = Completer();
+    _location = Location();
     super.initState();
   }
 
@@ -25,13 +28,34 @@ class _MapsScreenState extends State<MapsScreen> {
   Widget build(BuildContext context) {
     return GoogleMap(
       initialCameraPosition: CameraPosition(
-        target: LatLng(0, 0),
+        target: LatLng(30.0444, 31.2357),
         zoom: 14,
       ),
       mapType: MapType.normal,
       onMapCreated: (GoogleMapController controller) {
         _mapController.complete(controller);
+        _animateToCurrentPosition();
       },
+      myLocationEnabled: true,
+    );
+  }
+
+  Future<CameraPosition> _getCurrentCameraLocation() async {
+    final _currentLocation = await _location.getLocation();
+    final CameraPosition _currentCameraPostion = CameraPosition(
+      target:
+          LatLng(_currentLocation["latitude"], _currentLocation["longitude"]),
+      zoom: 16,
+    );
+    return _currentCameraPostion;
+  }
+
+  Future<void> _animateToCurrentPosition() async {
+    final CameraPosition _currentCameraPosition =
+        await _getCurrentCameraLocation();
+    final GoogleMapController _controller = await _mapController.future;
+    _controller.animateCamera(
+      CameraUpdate.newCameraPosition(_currentCameraPosition),
     );
   }
 }

43a32dc5e6fb066306b884bc0fa9ec8d1409f40e
Ali-Amin <ali.m.amin98@gmail.com>
2019-04-27 12:01:04 +0200

    Create auth service with sending sms code logic

---
+++ lib/src/services/firebase_auth.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthenticationService {
+  signInWithPhonenumber({
+    @required FirebaseAuth firebaseAuthInstance,
+    @required String phoneNumber,
+    @required Function(AuthException) onVerificationFailed,
+    @required Function(FirebaseUser) onVerifictionCompleted,
+    @required Function(String) onAutoRetrievalTimeout,
+    @required Function(String, [int]) onCodeSent,
+  }) {
+    return firebaseAuthInstance.verifyPhoneNumber(
+      phoneNumber: '+2$phoneNumber',
+      forceResendingToken: 1,
+      verificationFailed: onVerificationFailed,
+      verificationCompleted: onVerifictionCompleted,
+      codeAutoRetrievalTimeout: onAutoRetrievalTimeout,
+      codeSent: onCodeSent,
+      timeout: Duration(seconds: 30),
+    );
+  }
+}

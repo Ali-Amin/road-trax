221ec239255cc1467e2349825779a5e12b3ce252
Ali-Amin <ali.m.amin98@gmail.com>
2019-04-27 21:33:19 +0200

    Enable loading on rounded button

--- lib/src/common/custom_rounded_button.dart
+++ lib/src/common/custom_rounded_button.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class CustomRoundedButton extends StatelessWidget {
   final String text;
   final Function onPressed;
-  const CustomRoundedButton({Key key, @required this.text, this.onPressed})
-      : super(key: key);
+  final bool isLoading;
+  const CustomRoundedButton({
+    Key key,
+    @required this.text,
+    this.onPressed,
+    this.isLoading = false,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +20,12 @@ class CustomRoundedButton extends StatelessWidget {
       child: FlatButton(
         shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.all(Radius.circular(200))),
-        child: Text(text),
+        child: isLoading
+            ? SpinKitWave(
+                color: Colors.white,
+                size: 20.0,
+              )
+            : Text(text),
         color: Color(0xFFF60068),
         textColor: Colors.white,
         onPressed: () {

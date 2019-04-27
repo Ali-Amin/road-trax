cf772f86e235e508a16885efed75a55206ee6039
Ali-Amin <ali.m.amin98@gmail.com>
2019-04-27 20:56:30 +0200

    Create navigation gaurding using a page controller

--- lib/src/screens/login/login_screen.dart
+++ lib/src/screens/login/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:roadtrax/src/blocs/bloc.dart';
 import 'package:provider/provider.dart';
 import 'package:roadtrax/src/common/common.dart';
+import 'package:roadtrax/src/models/models.dart';
 import 'package:roadtrax/src/screens/login/phone_login_screen.dart';
 import 'package:roadtrax/src/screens/login/sign_up_screen.dart';
 import 'package:roadtrax/src/screens/login/sms_verification_screen.dart';
@@ -32,6 +33,30 @@ class _LoginScreensState extends State<LoginScreens> {
 
   @override
   Widget build(BuildContext context) {
+    Bloc bloc = Provider.of<Bloc>(context);
+    bloc.authState$.listen((AuthState authState) {
+      switch (authState) {
+        case AuthState.SmsSent:
+          cont1.animateTo(
+            MediaQuery.of(context).size.height,
+            duration: Duration(milliseconds: 1000),
+            curve: Curves.decelerate,
+          );
+          break;
+        case AuthState.UserDoesNotExist:
+          cont2.animateTo(
+            MediaQuery.of(context).size.width,
+            duration: Duration(milliseconds: 1000),
+            curve: Curves.decelerate,
+          );
+          break;
+        case AuthState.Authenticated:
+          break;
+          break;
+        default:
+          break;
+      }
+    });
     return Scaffold(
       backgroundColor: Color(0xFF121A27),
       body: PageView(

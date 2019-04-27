a07e849fa9492282a1eb63210404432aa88454be
Ali-Amin <ali.m.amin98@gmail.com>
2019-04-27 14:57:26 +0200

    make login screen scrollable

--- lib/src/screens/phone_login/phone_login_screen.dart
+++ lib/src/screens/phone_login/phone_login_screen.dart
@@ -7,20 +7,26 @@ class PhoneLoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Icon(
-              Icons.check,
-              size: 200.0,
-              color: Colors.black,
-            ),
-            _PhoneNumberTextField(),
-            _SignInButton(),
-          ],
+      body: ListView(
+        controller: null,
+        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        padding: EdgeInsets.only(
+          left: 20,
+          right: 20.0,
+          top: MediaQuery.of(context).size.height * 0.1,
         ),
+        children: <Widget>[
+          Icon(
+            Icons.check,
+            size: 200.0,
+            color: Colors.black,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 70.0),
+            child: _PhoneNumberTextField(),
+          ),
+          _SignInButton(),
+        ],
       ),
     );
   }
@@ -30,7 +36,7 @@ class _PhoneNumberTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Bloc bloc = Provider.of<Bloc>(context);
-    return IconTextField(
+    return UnderLineTextField(
       icon: Icons.person_outline,
       hintText: "Phone Number eg. 01112345678",
       onChanged: bloc.changePhoneNumber,
@@ -40,7 +46,6 @@ class _PhoneNumberTextField extends StatelessWidget {
 
 class _SignInButton extends StatelessWidget {
   const _SignInButton({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return CustomRoundedButton(

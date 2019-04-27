9a711f37e08db99f3258547645bcd99c973e14c8
Ali-Amin <ali.m.amin98@gmail.com>
2019-04-27 15:47:15 +0200

    Replace placeholder container with scaffold

--- lib/src/screens/sms_verification/sms_verification_screen.dart
+++ lib/src/screens/sms_verification/sms_verification_screen.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class SmsVerificationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold();
   }
 }

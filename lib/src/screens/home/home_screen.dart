62e2a04cbe30e8381b1666e47170f9ed6c891278
KarimElghamry <Karimelghamry@gmail.com>
2019-04-27 13:54:41 +0200

    skeleton for home page

---
+++ lib/src/screens/home/home_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Color(0xFF1C222E),
+        title: Text("HomePage"),
+      ),
+    );
+  }
+}

695ad2430239a4ce88055ac090950f6c7edb3a33
Ali-Amin <ali.m.amin98@gmail.com>
2019-04-27 20:00:21 +0200

    Modularize Login Screens

---
+++ lib/src/screens/login/sms_verification_screen.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:roadtrax/src/blocs/bloc.dart';
+import 'package:roadtrax/src/common/common.dart';
+
+class SmsVerificationScreen extends StatefulWidget {
+  final ScrollController cont;
+  SmsVerificationScreen({this.cont});
+
+  @override
+  SmsVerificationScreenState createState() {
+    return new SmsVerificationScreenState();
+  }
+}
+
+class SmsVerificationScreenState extends State<SmsVerificationScreen> {
+  FocusNode textFieldFocusNode;
+  @override
+  void initState() {
+    textFieldFocusNode = FocusNode();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    textFieldFocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Bloc bloc = Provider.of<Bloc>(context);
+    return WillPopScope(
+      onWillPop: () async {
+        widget.cont.animateTo(
+          -MediaQuery.of(context).size.height * 0.1,
+          duration: Duration(milliseconds: 1000),
+          curve: Curves.decelerate,
+        );
+      },
+      child: Scaffold(
+        backgroundColor: Color(0xFF121A27),
+        body: ListView(
+          padding: EdgeInsets.only(
+            left: 20,
+            right: 20.0,
+            top: MediaQuery.of(context).size.height * 0.1,
+          ),
+          children: <Widget>[
+            Icon(
+              Icons.check,
+              size: 200.0,
+              color: Color(0xFFF60068),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 70.0),
+              child: _SmsVerificationTextField(
+                focusNode: textFieldFocusNode,
+              ),
+            ),
+            CustomRoundedButton(
+              text: "VERIFY CODE",
+              onPressed: () {
+                textFieldFocusNode.unfocus();
+                bloc.sendSmsCode();
+                widget.cont.animateTo(
+                  MediaQuery.of(context).size.height,
+                  duration: Duration(milliseconds: 1000),
+                  curve: Curves.decelerate,
+                );
+              },
+            )
+            // _SignInButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _SmsVerificationTextField extends StatelessWidget {
+  final FocusNode focusNode;
+  _SmsVerificationTextField({this.focusNode});
+  @override
+  Widget build(BuildContext context) {
+    Bloc bloc = Provider.of<Bloc>(context);
+    return UnderLineTextField(
+      focusNode: focusNode,
+      icon: Icons.mail_outline,
+      hintText: "eg. 123456",
+      label: "SMS Code",
+      onChanged: bloc.changePhoneNumber,
+    );
+  }
+}

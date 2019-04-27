558437433173dcbbb91a8397d147618504c5b6e5
KarimElghamry <Karimelghamry@gmail.com>
2019-04-27 22:22:51 +0200

    implemented dismiss

--- lib/src/screens/maps/map_songs_screen.dart
+++ lib/src/screens/maps/map_songs_screen.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:roadtrax/src/common/music_card.dart';
 
 class MapSongsScreen extends StatelessWidget {
-  final Map<String, num> _songs;
+  Map<String, num> _songs;
+  List<String> _songNames;
+  List<String> _songGenres;
+  List<num> _count;
 
-  MapSongsScreen({@required Map<String, num> songs}) : _songs = songs;
+  MapSongsScreen({@required Map<String, num> songs}) {
+    _songs = songs;
+    _songNames = [];
+    _songGenres = [];
+    _count = _songs.values.toList();
+    final List<String> _songsHeader = _songs.keys.toList();
+    _songsHeader.forEach(
+      (String key) {
+        final List<String> _temp = key.split(';');
+        _songNames.add(_temp[0]);
+        _songGenres.add(_temp[1]);
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF121A27),
+      backgroundColor: Color(0xFF1A1A1A),
       appBar: AppBar(
         elevation: 0.0,
-        backgroundColor: Color(0xFF1C222E),
+        backgroundColor: Colors.transparent,
         title: Text("Map Songs"),
         centerTitle: true,
         leading: IconButton(
@@ -21,6 +38,28 @@ class MapSongsScreen extends StatelessWidget {
           ),
           onPressed: () => Navigator.of(context).pop(),
         ),
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 16.0),
+        itemCount: _songs.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Dismissible(
+            background: Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: Container(
+                width: double.infinity,
+                height: double.infinity,
+                color: Color(0xFFFE0069),
+              ),
+            ),
+            key: Key(index.toString()),
+            child: MusicCard(
+              index: index + 1,
+              songName: _songNames[index],
+              genre: _songGenres[index],
+            ),
+          );
+        },
       ),
     );
   }

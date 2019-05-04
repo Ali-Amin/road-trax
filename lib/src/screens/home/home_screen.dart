0f23d0cd43e43a920faeef9e98aecb4c5da3d8e4
KarimElghamry <Karimelghamry@gmail.com>
2019-05-04 18:17:31 +0200

    local music screen addition to home screen

--- lib/src/screens/home/home_screen.dart
+++ lib/src/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fancy_bottom_navigation/fancy_bottom_navigation.dart';
 import 'package:roadtrax/src/screens/home/home_screen_bloc.dart';
+import 'package:roadtrax/src/screens/local_music/local_music_screen.dart';
 import '../maps/maps.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -91,9 +92,7 @@ class _HomeScreenState extends State<HomeScreen> {
               return MapsScreen();
               break;
             case 2:
-              return Container(
-                color: Colors.green,
-              );
+              return LocalMusicScreen();
               break;
             default:
               return CircularProgressIndicator();

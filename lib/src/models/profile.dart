b2ae38db1bd31cb0409f0887a8e82bd078dc9e24
Ali-Amin <ali.m.amin98@gmail.com>
2019-12-14 22:19:41 +0200

    add avgSongScores and listenHistory to profile

--- lib/src/models/profile.dart
+++ lib/src/models/profile.dart
@@ -4,16 +4,47 @@ class Profile {
   String _uid;
   String _phoneNumber;
   String _userName;
+  Map<String, dynamic> _avgSongScores;
+  Map<String, num> _listenHistory;
 
   String get uid => _uid;
   String get phoneNumber => _phoneNumber;
   String get userName => _userName;
+  Map<String, dynamic> get avgSongScores => _avgSongScores;
+  Map<String, num> get listenHistory => _listenHistory;
 
-  Profile._(
-    this._uid,
-    this._phoneNumber,
-    this._userName,
-  );
+  Profile addSongToHistory(String songName) {
+    String formattedSongName = songName.trim();
+    if (formattedSongName.contains("-")) {
+      formattedSongName = formattedSongName.split("-")[1];
+    }
+
+    if (formattedSongName.contains("'")) {
+      formattedSongName = formattedSongName.replaceAll("'", "");
+    }
+
+    if (formattedSongName.contains("\"")) {
+      formattedSongName = formattedSongName.replaceAll("\"", "");
+    }
+    if (_listenHistory[formattedSongName] == null) {
+      this._listenHistory[formattedSongName] = 1;
+    } else {
+      this._listenHistory[formattedSongName]++;
+    }
+    return this;
+  }
+
+  Profile._({
+    String uid,
+    String phoneNumber,
+    String userName,
+    Map<String, dynamic> avgSongScores,
+    Map<String, num> listenHistory,
+  })  : _uid = uid,
+        _phoneNumber = phoneNumber,
+        _userName = userName,
+        _avgSongScores = avgSongScores,
+        _listenHistory = listenHistory;
 
   factory Profile.fromDocument(DocumentSnapshot doc) {
     if (doc == null) {
@@ -21,9 +52,13 @@ class Profile {
     } else {
       Map<dynamic, dynamic> profileData = doc.data;
       return Profile._(
-        doc.documentID,
-        profileData['phoneNumber'],
-        profileData['name'],
+        uid: doc.documentID,
+        phoneNumber: profileData['phoneNumber'],
+        userName: profileData['name'],
+        avgSongScores: Map.castFrom<dynamic, dynamic, String, dynamic>(
+            profileData['averageScore']),
+        listenHistory: Map.castFrom<dynamic, dynamic, String, num>(
+            profileData['listenHistory']),
       );
     }
   }
